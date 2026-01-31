b8d4b61ace4637ab87975e97d4afc95e54baf779
El Mehdi El Haddad <elmehdi.elhaddad@icloud.com>
2026-01-31 23:00:54 +0100

    Easy form

--- contact.php
+++ contact.php
@@ -1,6 +1,25 @@
 <article id="contact">
     <h2 class="major">Contact</h2>
-    <p>Feel free to get in touch via email or follow the latest updates on Instagram.</p>
+    <form method="post" action="#">
+        <div class="fields">
+            <div class="field half">
+                <label for="name">Name</label>
+                <input type="text" name="name" id="name" required />
+            </div>
+            <div class="field half">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" required />
+            </div>
+            <div class="field">
+                <label for="message">Message</label>
+                <textarea name="message" id="message" rows="4" required></textarea>
+            </div>
+        </div>
+        <ul class="actions">
+            <li><input type="submit" name="contact_submit" value="Send Message" class="primary" /></li>
+            <li><input type="reset" value="Reset" /></li>
+        </ul>
+    </form>
     <ul class="icons">
         <li>
             <a href="mailto:contact@feuzanderie.fr" 
